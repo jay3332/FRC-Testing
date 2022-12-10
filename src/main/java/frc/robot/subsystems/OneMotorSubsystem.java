5b4afa691b8eead59b03aed6c2cc51ef3724a3c8
jay3332 <40323796+jay3332@users.noreply.github.com>
2022-12-09 19:34:57 -0500

    initial commit

---
+++ src/main/java/frc/robot/subsystems/OneMotorSubsystem.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class OneMotorSubsystem extends SubsystemBase {
+    private final CANSparkMax spark = new CANSparkMax(13, MotorType.kBrushless);
+    public double power = 0.0;
+
+    /** Creates a new ExampleSubsystem. */
+    public OneMotorSubsystem() {
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run 
+        spark.set(power);
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+}
